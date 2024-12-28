c5c5b66670dcaeb144ddd2082a765f7e3673512d
Sanjib Sinha <sanjib12sinha@gmail.com>
2024-12-28 07:56:13 +0530

    func commit again

---
+++ func.py
@@ -0,0 +1,26 @@
+def greet(name):
+    """
+    Greet a person with a hello message.
+    Args:
+        name (str): The name of the person to greet.
+    Returns:
+        str: A greeting message.
+    """
+
+    return f'Hello, {name}!'
+
+print(greet('Sanjib')) # greet('Hello World')
+
+def add(a, b):
+    """
+    Adds two numbers together.
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+    Returns:
+        int or float: The sum of the two numbers.
+    """
+    
+    return a + b
+
+print(add(2, 3)) # add(2, 3)
